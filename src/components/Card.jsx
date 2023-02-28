781fc8ef78777a471ac77021388058ac89780898
asifmuntasir <shuyeb.dk@gmail.com>
2023-02-28 22:23:07 +0600

    total world data displayed successfully on UI

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const Card = ({ img }) => {
+const Card = ({ img, title, data }) => {
     return (
-        <div className="card mb-3" style={{ "maxWidth": "540px" }}>
+        <div className="card mb-3" style={{ "maxWidth": "550px" }}>
             <div className="row g-0">
                 <div className="col-md-5">
                     <img src={img} className="img-fluid h-100 rounded-start" alt="..." />
                 </div>
                 <div className="col-md-7">
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <h5 className="card-title fs-2">{title}</h5>
+                        <p className="card-text fs-5 text-warning--dark">{data}</p>
                         <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
                     </div>
                 </div>
--- src/components/TotalCaseComponent.jsx
+++ src/components/TotalCaseComponent.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Card from './Card';
 
 const imageLink = {
@@ -12,28 +13,67 @@ const imageLink = {
 
 
 
+
 const TotalCaseComponent = () => {
+
+    const [info, setInfo] = useState({})
+
+    useEffect(() => {
+        async function getData() {
+            const response = await axios.get('https://disease.sh/v3/covid-19/all');
+            // console.log(response.data);
+            setInfo(response.data);
+        }
+
+        getData();
+    }, [])
+
+
     return (
-        <div className="container fluid">
+        <div className="container fluid py-3">
             <div className="container-md">
                 <div className="row row-cols-3">
                     <div className="col">
-                        <Card img={imageLink.world} />
+                        <Card
+                            img={imageLink.world}
+                            title={'Total Update'}
+                            data={info.updated}
+                        />
                     </div>
                     <div className="col">
-                        <Card img={imageLink.totalCases} />
+                        <Card
+                            img={imageLink.totalCases}
+                            title={'Total Cases'}
+                            data={info.cases}
+                        />
                     </div>
                     <div className="col">
-                        <Card img={imageLink.totalDeaths} />
+                        <Card
+                            img={imageLink.totalDeaths}
+                            title={'Total Deaths'}
+                            data={info.deaths}
+                        />
                     </div>
                     <div className="col">
-                        <Card img={imageLink.todayCases} />
+                        <Card
+                            img={imageLink.todayCases}
+                            title={'Today Cases'}
+                            data={info.todayCases}
+                        />
                     </div>
                     <div className="col">
-                        <Card img={imageLink.recovery} />
+                        <Card
+                            img={imageLink.recovery}
+                            title={'Today Recovery'}
+                            data={info.todayRecovered}
+                        />
                     </div>
                     <div className="col">
-                        <Card img={imageLink.todayDeaths} />
+                        <Card
+                            img={imageLink.todayDeaths}
+                            title={'Today Deaths'}
+                            data={info.todayDeaths}
+                        />
                     </div>
                 </div>
             </div>
